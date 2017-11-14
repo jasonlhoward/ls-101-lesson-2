1fbb17a5853e5fcab0d0d8b5aea646fc1fbdb304
Jason Howard <jasonlhoward@gmail.com>
2017-11-14 12:32:51 -0700

    Fixed too many bugs to list

--- rpsls.rb
+++ rpsls.rb
@@ -1,67 +1,72 @@
 VALID_CHOICES = %w[rock paper scissors lizard Spock].freeze()
 
+WINS = {
+  'rock' => %w[scissors lizard],
+  'paper' => %w[rock Spock],
+  'scissors' => %w[paper lizard],
+  'lizard' => %w[Spock paper],
+  'Spock' => %w[scissors rock]
+}.freeze
+
+WIN_VERBS = {
+  'rock' => {
+    'scissors' => 'crushes',
+    'lizard' => 'crushes'
+  },
+  'paper' => {
+    'rock' => 'covers',
+    'Spock' => 'disproves'
+  },
+  'scissors' => {
+    'paper' => 'cuts',
+    'lizard' => 'decapitates'
+  },
+  'lizard' => {
+    'Spock' => 'poisons',
+    'paper' => 'eats'
+  },
+  'Spock' => {
+    'scissors' => 'smashes',
+    'rock' => 'vaporizes'
+  }
+}.freeze
+
 def win?(first, second)
-  case first
-  when 'rock'
-    second == 'scissors' || second == 'lizard'
-  when 'paper'
-    second == 'rock' || second == 'Spock'
-  when 'scissors'
-    second == 'paper' || second == 'lizard'
-  when 'lizard'
-    second == 'Spock' || second == 'paper'
-  when 'Spock'
-    second == 'scissors' || second == 'rock'
-  end
+  WINS[first].include?(second)
 end
 
-def win_verb(first, second)
-  case first
-  when 'paper'
-    second == 'rock' ? 'covers' : 'disproves'
-  when 'scissors'
-    second == 'paper' ? 'cuts' : 'decapitates'
-  when 'lizard'
-    second == 'Spock' ? 'poisons' : 'eats'
-  when 'Spock'
-    second == 'scissors' ? 'smashes' : 'vaporizes'
-  else # rock always crushes
-    'crushes'
-  end
+def win_verb(winner, loser)
+  WIN_VERBS[winner][loser]
 end
 
-def display_results(player, computer, score)
+def display_results(player, computer)
   prompt("You chose: #{player}; Computer chose: #{computer}")
   if win?(player, computer)
-    update_score(0, score)
     prompt("#{player} #{win_verb(player, computer)} #{computer}")
     prompt('You win!')
   elsif win?(computer, player)
-    update_score(1, score)
     prompt("#{computer} #{win_verb(computer, player)} #{player}")
     prompt('Computer wins!')
   else
     prompt("It's a tie!")
   end
-  display_score(score)
 end
 
 def display_score(score)
-  if score[0] > score[1]
-    prompt("You're winning! #{score[0]} to #{score[1]}")
+  player_score = score[0]
+  computer_score = score[1]
+  if player_score > computer_score
+    prompt("You're winning #{player_score} to #{computer_score}!")
   elsif score[1] > score[0]
-    prompt("The computer is winning! #{score[1]} to #{score[0]}")
+    prompt("The computer is winning #{computer_score} to #{player_score}!")
   else
-    prompt("The score is tied! #{score[0]} to #{score[1]}")
+    prompt("The score is tied #{player_score} to #{computer_score}!")
   end
 end
 
-def update_score(winner, score)
-  if winner.zero?
-    score[0] += 1
-  elsif winner == 1
-    score[1] += 1
-  end
+def update_score(player, computer, score)
+  score[0] += 1 if win?(player, computer)
+  score[1] += 1 if win?(computer, player)
 end
 
 def game_over?(score)
@@ -72,41 +77,47 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def player_choice
+def player_choose
   player = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    player = Kernel.gets().chomp()
-    if player.length == 1
-      player = VALID_CHOICES[VALID_CHOICES.map(&:chr).index(player)]
-      break if player
-    elsif VALID_CHOICES.include?(player)
-      break
-    end
-    prompt("That's not a valid choice.")
+    player = valid_player(Kernel.gets().chomp())
+    break if player
+    prompt('Invalid choice. Can also use r, p, s, l or S.')
   end
   player
 end
 
-def computer_choice
+def valid_player(player)
+  if player.length == 1 && VALID_CHOICES.map(&:chr).index(player)
+    player = VALID_CHOICES[VALID_CHOICES.map(&:chr).index(player)]
+    valid_player(player)
+  elsif VALID_CHOICES.include?(player)
+    player
+  else
+    false
+  end
+end
+
+def computer_choose
   VALID_CHOICES.sample()
 end
 
+prompt('Rock Paper Scissors Lizard Spock!')
+prompt('First to 5 wins ends the game!')
 score = [0, 0]
-loop do # main loop
-  display_results(player_choice, computer_choice, score)
+loop do
+  player = player_choose
+  computer = computer_choose
+  update_score(player, computer, score)
+  display_results(player, computer)
+  display_score(score)
 
-  if game_over?(score)
-    if score[0] >= 5
-      prompt('Game Over. You Won!')
-    else
-      prompt('Game Over. You Lost!')
-    end
-    break
+  next unless game_over?(score)
+  if score[0] >= 5
+    prompt('Game Over. You Won!')
   else
-    prompt('Do you want to keep playing? ("n" to forfeit)')
-    if Kernel.gets().chomp().downcase().start_with?('n')
-      break prompt('Game Over. You Forfeited!')
-    end
+    prompt('Game Over. You Lost!')
   end
+  break
 end
